c485f88f637ceb55cbf65b80773a19c0af0a2e4f
laderakara <karaladera@gmail.com>
2020-11-18 01:05:51 +0800

    salamat master

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -5,12 +5,12 @@ use Illuminate\Http\Response;
 trait ApiResponser
 {
     
-    protected function successResponse($data, $code = Response:: HTTP_OK)
+    public function successResponse($data, $code = Response:: HTTP_OK)
     {
         return response() -> json(['data'=>$data], $code);
     }
 
-    protected function errorResponse($message, $code)
+    public function errorResponse($message, $code)
     {
         return response() -> json(['error'=>$message, 'code' =>$code], $code);
     }
